26d20dd6581b96450ee15510612cf09391df8b7b
Robin <mohammadrobin636@gmail.com>
2023-05-16 22:48:20 +0600

    First commit

---
+++ index.js
@@ -0,0 +1,64 @@
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const express = require('express');
+const cors = require('cors');
+const app = express();
+const port = process.env.PORT || 3000;
+require('dotenv').config();
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.061zho5.mongodb.net/?retryWrites=true&w=majority`;
+
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const database = client.db('Foot-Frenzy');
+    const productCollection = database.collection('products');
+
+    app.get('/products', async (req, res) => {
+      const page = parseInt(req.query.page ) || 0;
+      const limit = parseInt(req.query.limit) || 10;
+      const skip = page * limit;
+      const products  = await productCollection.find().skip(skip).limit(limit).toArray();
+      res.send(products);
+    })
+    app.get('/total-products', async(req, res) => {
+      const totalProducts = await productCollection.estimatedDocumentCount();
+      res.send({totalProducts: totalProducts});
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('Ema-John Server running');
+})
+
+app.listen(port, () => {
+    console.log('Server is running on port', port);
+})
